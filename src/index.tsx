a6ecb2d6f176db5686e3268b3d0c372718d127c1
Vivi <vivizengjunyi@gmail.com>
2024-04-03 21:21:16 -0700

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./reducer/store";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <Router basename="/weather">
+        <Switch>
+          <Route path="/" component={App}></Route>
+        </Switch>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
